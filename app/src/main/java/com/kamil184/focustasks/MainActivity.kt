459a9c94651441037c4923c53fd00d3f3bdfc504
kamil184 <45402327+kamil184@users.noreply.github.com>
2022-07-14 19:23:33 +0200

    Insets added

--- app/src/main/java/com/kamil184/focustasks/MainActivity.kt
+++ app/src/main/java/com/kamil184/focustasks/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.kamil184.focustasks
 
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsetsController
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.*
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -14,10 +17,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        window.setDecorFitsSystemWindows(false)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        window.insetsController?.setSystemBarsAppearance(
+            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+        )
+
         val navView: BottomNavigationView = binding.navView
+
+        ViewCompat
+            .setOnApplyWindowInsetsListener(navView) { view, insets ->
+                view.updatePadding(bottom = insets.getInsets(WindowInsetsCompat.Type.systemBars()).bottom)
+                insets
+            }
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
--- app/src/main/java/com/kamil184/focustasks/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/kamil184/focustasks/ui/tasks/TasksFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.kamil184.focustasks.databinding.FragmentTasksBinding
@@ -24,6 +27,12 @@ class TasksFragment : Fragment() {
             ViewModelProvider(this).get(TasksViewModel::class.java)
 
         _binding = FragmentTasksBinding.inflate(inflater, container, false)
+
+        ViewCompat
+            .setOnApplyWindowInsetsListener(binding.tasksTabLayout) { view, insets ->
+                view.updatePadding(top = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top)
+                insets
+            }
 
         binding.tasksFab.setOnClickListener {
             val taskCreateBottomSheet = TaskCreateBottomSheet()
