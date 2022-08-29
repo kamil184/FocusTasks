f307aa7a13053bfae7ba48b25f2374e123c0cb2b
kamil184 <45402327+kamil184@users.noreply.github.com>
2022-08-29 23:19:03 +0200

    DatePickerViewModel.kt getTimeText fix

--- app/src/main/java/com/kamil184/focustasks/ui/tasks/DatePickerViewModel.kt
+++ app/src/main/java/com/kamil184/focustasks/ui/tasks/DatePickerViewModel.kt
@@ -8,17 +8,23 @@ class DatePickerViewModel : ViewModel() {
 
     val repeat = MutableLiveData<Repeat?>()
 
-    fun getTimeText(hour: Int, minute: Int, isSystem24Hour: Boolean) =
-        if (isSystem24Hour) {
-            "${hour}:${minute}"
+    fun getTimeText(hour: Int, minute: Int, isSystem24Hour: Boolean): String {
+        val m =
+            if (minute < 10) "0$minute" else "$minute"
+
+        return if (isSystem24Hour) {
+            val h =
+                if (hour < 10) "0$hour" else "$hour"
+            "$h:$m"
         } else {
             val isAm = hour < 12
-            var h = if (isAm) hour
+            var hAmPm = if (isAm) hour
             else hour - 12
-            if (h == 0) h = 12
+            if (hAmPm == 0) hAmPm = 12
+            val h =
+                if (hAmPm < 10) "0$hAmPm" else "$hAmPm"
             val amPmText = if (isAm) "AM" else "PM"
-            val m =
-                if (minute < 10) "0${minute}" else "${minute}"
             "$amPmText $h:$m"
         }
+    }
 }
